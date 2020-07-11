b70650e12070b299260ad1acc4b69e62fb9651c7
Tony Sorensen <tony48853@gmail.com>
2020-07-11 11:10:02 -0700

    added right click scale change to images

--- js/index.js
+++ js/index.js
@@ -1,29 +1,50 @@
 // Your code goes here
-const subhead = document.querySelectorAll('h2')
-subhead.forEach(subhead => {
-    subhead.addEventListener('mouseenter', (event) => {
-        subhead.style.transform = 'scale(1.25)'
-    subhead.addEventListener('mouseleave', () => {
-        subhead.style.transform = 'scale(1)'
-        
-})
+const subhead = document.querySelectorAll("h2");
+subhead.forEach((subhead) => {
+  subhead.addEventListener("mouseenter", () => {
+    subhead.style.transform = "scale(1.25)";
+    subhead.addEventListener("mouseleave", () => {
+      subhead.style.transform = "scale(1)";
+    });
+  });
+});
+
+const body = document.querySelector("body");
+
+body.addEventListener("click", () => {
+  body.style.backgroundColor = "yellow";
+});
+
+const links = document.querySelectorAll("a");
+links.forEach((links) => {
+  links.addEventListener("mouseover", () => {
+    links.style.color = "blue";
+    links.style.fontSize = "2.5em";
+  });
+});
+
+const img = document.querySelectorAll('img')
+img.forEach((img) => { 
+    img.addEventListener('contextmenu', ()=> {
+        img.style.transform = "scale(.75)"
     })
-
 })
 
 
 
-const body = document.querySelector('body')
 
-body.addEventListener('click', () => {
-    body.style.backgroundColor = "yellow"
-    
-})
 
-const links = document.querySelectorAll('a')
-links.forEach(links => {
-    links.addEventListener('mouseover', () => {
-    links.style.color = 'blue'
-    links.style.fontSize = '2.5em'
-})
-})
+
+
+
+    // let scale = 1;
+//     img.addEventListener("wheel", () => {
+//         //this prevents the wheel from scrolling the page when it is centered on the image
+//         event.preventDefault();
+//         //this sets the min and max of the image size
+//         scale = Math.min(Math.max(.125, scale), .947);
+//         //this sets the sensitivity of the scroll wheel
+//         scale += event.deltaY * .001;
+// img.style.transform = `scale(${scale})`;
+//     } )
+// })
