5a053142524e5ad8f4cdf0e8902a82cf9f148370
Tony Sorensen <tony48853@gmail.com>
2020-07-11 10:16:39 -0700

    Created 4 mouse events- enter and leave on main image, click on body element and mouseover on links

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 // Your code goes here
+const image = document.getElementsByTagName('img')
+image[0].addEventListener('mouseenter', () => {
+    image[0].style.transform = 'scale(1.15)'
+})
+
+
+image[0].addEventListener('mouseleave', () => {
+    image[0].style.transform = 'scale(1)'
+})
+
+
+const body = document.querySelector('body')
+
+body.addEventListener('click', () => {
+    body.style.backgroundColor = "yellow"
+    
+})
+
+const links = document.querySelectorAll('a')
+links.forEach(links => {
+    links.addEventListener('mouseover', () => {
+    links.style.color = 'blue'
+    links.style.fontSize = '2.5em'
+})
+})
